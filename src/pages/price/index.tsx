70ca61931842e90e95e84dc4e6cf9d78a6a2c71a
annamarcomini <annajuliamarcomini23@gmail.com>
2024-05-26 20:58:50 -0300

    style: adjusting responsive styles on price page

--- src/pages/price/index.tsx
+++ src/pages/price/index.tsx
@@ -5,7 +5,6 @@ import Head from "next/head";
 import { useVehicleContext } from "@/contexts/Vehicle";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import fipeApi from "@/service/fipeApi";
 
 const FullHeightContainer = styled(Box)(
   ({ theme }) => css`
@@ -24,7 +23,7 @@ const Title = styled(Typography)`
   text-align: center;
 
   ${({ theme }) => theme.breakpoints.down("sm")} {
-    font-size: 40px;
+    font-size: 2rem;
   }
 `;
 
@@ -43,9 +42,13 @@ const PriceText = styled(Typography)(
     align-items: center;
     justify-content: center;
     text-align: center;
-    font-size: 1.9rem;
+    font-size: 1.5rem;
     font-weight: bold;
     color: ${theme.palette.secondary.contrastText};
+
+    ${theme.breakpoints.down("sm")} {
+      font-size: 1rem;
+    }
   `
 );
 
@@ -76,15 +79,19 @@ export default function Price() {
       <FullHeightContainer>
         <Grid container justifyContent={"center"} mt={2}>
           <Grid item xs={12} sm={8} md={6} lg={6}>
-            <Title variant="h4">
+            <Title sx={{ px: 3 }} variant="h4">
               {`Tabela Fipe: Preço ${selectedBrand?.nome} ${selectedModel?.nome}
           ${selectedYear?.nome}`}
             </Title>
           </Grid>
         </Grid>
-        <PriceContainer mt={3} px={3} py={1}>
-          <PriceText>{price}</PriceText>
-        </PriceContainer>
+        <Grid container justifyContent={"center"} mt={2}>
+          <Grid item xs={6} sm={6} md={6} lg={2}>
+            <PriceContainer mt={3} px={3} py={1}>
+              <PriceText>{price}</PriceText>
+            </PriceContainer>
+          </Grid>
+        </Grid>
         <Label mt={2}>Este é o preço de compra do veículo</Label>
       </FullHeightContainer>
     </>
